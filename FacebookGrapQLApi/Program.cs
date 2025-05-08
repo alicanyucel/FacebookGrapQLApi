84ff40232107b5f2451d7447a26c2d319972e9e1
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-05-08 14:45:36 +0300

    access token sorunu var..

--- FacebookGrapQLApi/Program.cs
+++ FacebookGrapQLApi/Program.cs
@@ -1,21 +1,28 @@
+using FacebookGrapQLApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddHttpClient();
+builder.Services.AddEndpointsApiExplorer();           
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<FacebookSettings>(
+    builder.Configuration.GetSection("Facebook"));
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
