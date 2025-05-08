693af2b38563a25dcd6fb43906b10d7f3546ab28
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-05-08 15:44:51 +0300

    //

--- FacebookGrapQLApi/Controllers/FacebookController.cs
+++ FacebookGrapQLApi/Controllers/FacebookController.cs
@@ -1,60 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
+﻿using FacebookGrapQLApi.Models;
 using FacebookUserInfoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
-using FacebookGrapQLApi.Models;
-using Microsoft.AspNetCore.Authorization;
 
-namespace FacebookUserInfoApi.Controllers
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class FacebookController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]/[action]")]
-    [AllowAnonymous]
-    public class FacebookController : ControllerBase
+    private readonly HttpClient _httpClient;
+    private readonly FacebookSettings _fbSettings;
+
+    public FacebookController(IHttpClientFactory factory, IOptions<FacebookSettings> options)
     {
-        private readonly HttpClient _httpClient;
-        private readonly FacebookSettings _fbSettings;
+        _httpClient = factory.CreateClient();
+        _fbSettings = options.Value;
+    }
 
-        public FacebookController(IHttpClientFactory factory, IOptions<FacebookSettings> options)
+    [HttpGet]
+    public async Task<IActionResult> GetFacebookUser([FromQuery] string userAccessToken)
+    {
+        if (string.IsNullOrWhiteSpace(userAccessToken))
+            return BadRequest("User access token is required");
+        var appTokenUrl = $"https://graph.facebook.com/oauth/access_token" +
+                          $"?client_id={_fbSettings.AppId}" +
+                          $"&client_secret={_fbSettings.AppSecret}" +
+                          $"&grant_type=client_credentials";
+
+        var appTokenResponse = await _httpClient.GetAsync(appTokenUrl);
+        var appTokenContent = await appTokenResponse.Content.ReadAsStringAsync();
+        var appTokenJson = JObject.Parse(appTokenContent);
+        var appAccessToken = appTokenJson["access_token"]?.ToString();
+        var debugUrl = $"https://graph.facebook.com/debug_token" +
+                       $"?input_token={userAccessToken}" +
+                       $"&access_token={appAccessToken}";
+
+        var debugResponse = await _httpClient.GetAsync(debugUrl);
+        if (!debugResponse.IsSuccessStatusCode)
+            return Unauthorized("Invalid or expired Facebook user access token.");
+        var userInfoUrl = $"https://graph.facebook.com/me?fields=id,name,email&access_token={userAccessToken}";
+        var userInfoResponse = await _httpClient.GetAsync(userInfoUrl);
+        var content = await userInfoResponse.Content.ReadAsStringAsync();
+        var json = JObject.Parse(content);
+
+        var user = new FacebookUserDto
         {
-            _httpClient = factory.CreateClient();
-            _fbSettings = options.Value;
-        }
-
-        [HttpGet("user")]
-        public async Task<IActionResult> GetFacebookUser([FromQuery] string userAccessToken)
-        {
-            if (string.IsNullOrWhiteSpace(userAccessToken))
-                return BadRequest("User access token is required");
-            var appTokenUrl = $"https://graph.facebook.com/oauth/access_token" +
-                              $"?client_id={_fbSettings.AppId}" +
-                              $"&client_secret={_fbSettings.AppSecret}" +
-                              $"&grant_type=client_credentials";
-
-            var appTokenResponse = await _httpClient.GetAsync(appTokenUrl);
-            var appTokenContent = await appTokenResponse.Content.ReadAsStringAsync();
-            var appTokenJson = JObject.Parse(appTokenContent);
-            var appAccessToken = appTokenJson["access_token"]?.ToString();
-            var debugUrl = $"https://graph.facebook.com/debug_token" +
-                           $"?input_token={userAccessToken}" +
-                           $"&access_token={appAccessToken}";
-
-            var debugResponse = await _httpClient.GetAsync(debugUrl);
-            if (!debugResponse.IsSuccessStatusCode)
-                return Unauthorized("Invalid or expired Facebook user access token.");
-            var userInfoUrl = $"https://graph.facebook.com/me?fields=id,name,email&access_token={userAccessToken}";
-            var userInfoResponse = await _httpClient.GetAsync(userInfoUrl);
-            var content = await userInfoResponse.Content.ReadAsStringAsync();
-            var json = JObject.Parse(content);
-
-            var user = new FacebookUserDto
-            {
-                Id = json["id"]?.ToString(),
-                Name = json["name"]?.ToString(),
-                Email = json["email"]?.ToString()
-            };
-
-            return Ok(user);
-        }
+            Id = json["id"]?.ToString(),
+            Name = json["name"]?.ToString(),
+            Email = json["email"]?.ToString()
+        };
+        // bakııym ....
+        return Ok(user);
     }
 }
